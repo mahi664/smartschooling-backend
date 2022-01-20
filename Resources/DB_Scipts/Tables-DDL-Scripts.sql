e89e23ed2badb854560babfc09259000109d62c6
Mahesh Ghuge <mahighuge664@gmail.com>
2022-01-20 09:34:45 +0530

    2 : DDL Scripts for the tables added for smart schooling back end development

--- Resources/DB_Scipts/Tables-DDL-Scripts.sql
+++ Resources/DB_Scipts/Tables-DDL-Scripts.sql
@@ -1,396 +1,171 @@
-DROP TABLE purchase_order_det;
-DROP TABLE vendor_receipt_map;
-DROP TABLE vendor_det;
-DROP TABLE product_igst_map;
-DROP TABLE product_cgst_map;
-DROP TABLE product_sgst_map;
-DROP TABLE igst_rates;
-DROP TABLE sgst_rates;
-DROP TABLE cgst_rates;
-DROP TABLE return_order;
-DROP TABLE order_customer_map;
-DROP TABLE pending_payment;
-DROP TABLE customer_det;
-DROP TABLE order_det;
-DROP TABLE product_category_map;
-DROP TABLE category_det;
-DROP TABLE product_discount_map;
-DROP TABLE product_det;
+CREATE TABLE smartschoolingdev.institute_det (
+	institute_id varchar(100) NOT NULL,
+	institute_name varchar(500) NOT NULL,
+	foundation_date DATETIME NOT NULL,
+	address varchar(500) NOT NULL,
+	last_update_time DATETIME NOT null,
+	last_user varchar(100) NOT null,
+	CONSTRAINT institute_det_pk PRIMARY KEY (institute_id)
+);
 
-CREATE TABLE `product_det` (
-  `PRODUCT_ID` INT NOT NULL,
-  `PROUCT_NAME` VARCHAR(1000) NOT NULL,
-  `DISP_NAME` VARCHAR(500) NOT NULL,
-  `DESC` VARCHAR(1000) NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  `RF3` INT NULL,
-  `RF4` INT NULL,
-  `RF5` INT NULL,
-  PRIMARY KEY (`PRODUCT_ID`),
-  UNIQUE INDEX `DISP_NAME_UNIQUE` (`DISP_NAME` ASC));
+CREATE TABLE smartschoolingdev.institute_branch_det (
+	branch_id varchar(100) NOT NULL,
+	institute_id varchar(100) NOT NULL,
+	branch_name varchar(200) NOT NULL,
+	foundation_date DATETIME NOT NULL,
+	address varchar(200) NOT NULL,
+	last_update_time DATETIME NOT null,
+	last_user varchar(100) NOT null,
+	CONSTRAINT institute_branch_det_pk PRIMARY KEY (branch_id),
+	CONSTRAINT institute_branch_det_fk FOREIGN KEY (institute_id) REFERENCES smartschoolingdev.institute_det(institute_id)
+);
 
+CREATE TABLE smartschoolingdev.users (
+	user_type varchar(100) NOT NULL,
+	user_role varchar(100) NOT NULL,
+	user_description varchar(100) NOT NULL,
+	CONSTRAINT users_pk PRIMARY KEY (user_type)
+);
 
-CREATE TABLE `product_discount_map` (
-  `PRODUCT_ID` INT NOT NULL,
-  `DISCOUNT` DOUBLE NULL,
-  `EFF_DATE_SKEY` INT NOT NULL,
-  `END_DATE_SKEY` INT NOT NULL,
-  `LAST_UPDATE_TIME` DATETIME NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  PRIMARY KEY (`PRODUCT_ID`, `EFF_DATE_SKEY`, `END_DATE_SKEY`),
-  CONSTRAINT `FK2`
-    FOREIGN KEY (`PRODUCT_ID`)
-    REFERENCES `product_det` (`PRODUCT_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+CREATE TABLE smartschoolingdev.classes (
+	class_id varchar(100) NOT NULL,
+	class_name varchar(100) NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT NULL,
+	CONSTRAINT classes_pk PRIMARY KEY (class_id)
+);
 
+CREATE TABLE smartschoolingdev.branch_classes_details (
+	branch_id varchar(100) NOT NULL,
+	class_id varchar(100) NOT NULL,
+	eff_date DATETIME NOT NULL,
+	end_date DATETIME NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT NULL,
+	CONSTRAINT branch_classes_details_pk PRIMARY KEY (branch_id,class_id,eff_date,end_date),
+	CONSTRAINT branch_classes_details_fk FOREIGN KEY (branch_id) REFERENCES smartschoolingdev.institute_branch_det(branch_id),
+	CONSTRAINT branch_classes_details_fk_1 FOREIGN KEY (class_id) REFERENCES smartschoolingdev.classes(class_id)
+);
 
-CREATE TABLE `category_det` (
-  `CATEGORY_ID` INT NOT NULL,
-  `NAME` VARCHAR(500) NULL,
-  `DISP_NAME` VARCHAR(500) NULL,
-  `DESC` VARCHAR(1000) NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  PRIMARY KEY (`CATEGORY_ID`),
-  UNIQUE INDEX `NAME_UNIQUE` (`NAME` ASC));
+CREATE TABLE smartschoolingdev.subjects (
+	sub_id varchar(100) NOT NULL,
+	sub_name varchar(100) NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT null,
+	CONSTRAINT subjects_pk PRIMARY KEY (sub_id)
+);
 
+CREATE TABLE smartschoolingdev.routes (
+	route_id varchar(100) NOT NULL,
+	source varchar(100) NOT NULL,
+	destination varchar(100) NOT NULL,
+	distance DECIMAL NOT NULL,
+	CONSTRAINT routes_pk PRIMARY KEY (route_id)
+);
 
-CREATE TABLE `product_category_map` (
-  `PRODUCT_ID` INT NOT NULL,
-  `CATEGORY_ID` INT NOT NULL,
-  `EFF_DATE_SKEY` INT NOT NULL,
-  `END_DATE_SKEY` INT NOT NULL,
-  `LAST_UPDATE_TIME` DATETIME NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  `RF3` INT NULL,
-  PRIMARY KEY (`PRODUCT_ID`, `CATEGORY_ID`, `EFF_DATE_SKEY`, `END_DATE_SKEY`),
-  INDEX `FK5_idx` (`CATEGORY_ID` ASC) ,
-  CONSTRAINT `FK4`
-    FOREIGN KEY (`PRODUCT_ID`)
-    REFERENCES `product_det` (`PRODUCT_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `FK5`
-    FOREIGN KEY (`CATEGORY_ID`)
-    REFERENCES `category_det` (`CATEGORY_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
+ALTER TABLE smartschoolingdev.routes ADD last_update_time DATETIME NOT NULL;
+ALTER TABLE smartschoolingdev.routes ADD last_user varchar(100) NOT NULL;
+ALTER TABLE smartschoolingdev.routes ADD CONSTRAINT routes_un UNIQUE KEY (route_id,source,destination,distance);
 
 
-CREATE TABLE `order_det` (
-  `ORDER_ID` INT NOT NULL,
-  `PRODUCT_ID` INT NOT NULL,
-  `QUANTITY` INT NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  `RF3` INT NULL,
-  PRIMARY KEY (`ORDER_ID`, `PRODUCT_ID`),
-  INDEX `FK7_idx` (`PRODUCT_ID` ASC),
-  CONSTRAINT `FK7`
-    FOREIGN KEY (`PRODUCT_ID`)
-    REFERENCES `product_det` (`PRODUCT_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
-CREATE TABLE `customer_det` (
-  `CUSTOMER_ID` INT NOT NULL,
-  `NAME` VARCHAR(500) NULL,
-  `PHONE` VARCHAR(12) NOT NULL,
-  `EMAIL` VARCHAR(500) NULL,
-  `ADDRESS` VARCHAR(500) NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  `RF3` INT NULL,
-  PRIMARY KEY (`CUSTOMER_ID`,`PHONE`));
-
-
-CREATE TABLE `pending_payment` (
-  `ORDER_ID` INT NOT NULL,
-  `PAID` DOUBLE NULL,
-  `UNPAID` DOUBLE NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  PRIMARY KEY (`ORDER_ID`),
-  CONSTRAINT `FK8`
-    FOREIGN KEY (`ORDER_ID`)
-    REFERENCES `order_det` (`ORDER_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
-CREATE TABLE `order_customer_map` (
-  `ORDER_ID` INT NOT NULL,
-  `CUSTOMET_ID` INT NOT NULL,
-  `DATE_SKEY` INT NOT NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  PRIMARY KEY (`ORDER_ID`, `CUSTOMET_ID`, `DATE_SKEY`),
-  INDEX `FK11_idx` (`CUSTOMET_ID` ASC),
-  CONSTRAINT `FK10`
-    FOREIGN KEY (`ORDER_ID`)
-    REFERENCES `order_det` (`ORDER_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `FK11`
-    FOREIGN KEY (`CUSTOMET_ID`)
-    REFERENCES `customer_det` (`CUSTOMER_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-
-CREATE TABLE `return_order` (
-  `NEW_ORDER_ID` INT NOT NULL,
-  `RETURN_TYPE` CHAR NULL,
-  `OLD_ORDER_ID` INT NOT NULL,
-  `DATE_SKEY` INT NOT NULL,
-  `RF1` INT NULL,
-  `RF2` INT NULL,
-  PRIMARY KEY (`NEW_ORDER_ID`, `DATE_SKEY`, `OLD_ORDER_ID`),
-  INDEX `FK12_idx` (`NEW_ORDER_ID` ASC, `OLD_ORDER_ID` ASC) ,
-  CONSTRAINT `FK12`
-    FOREIGN KEY (`NEW_ORDER_ID`)
-    REFERENCES `order_det` (`ORDER_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `FK13`
-    FOREIGN KEY (`OLD_ORDER_ID`)
-    REFERENCES `order_det` (`ORDER_ID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION);
-
-ALTER TABLE product_det CHANGE `prouct_name` `PRODUCT_NAME` varchar(1000);
-ALTER TABLE product_det CHANGE `DESC` `PRODUCT_DESC` varchar(1000);
-ALTER TABLE category_det CHANGE `DESC` `CATEGORY_DESC` varchar(1000);
-
-
-CREATE TABLE `cgst_rates` ( 
-	`GST_RATE`	double not NULL,
-	`NAME`    	varchar(100) NULL,
-	PRIMARY KEY(`GST_RATE`)
+CREATE TABLE smartschoolingdev.fee_types (
+	fee_id varchar(100) NOT NULL,
+	fee_name varchar(100) NOT NULL,
+	fee_discription varchar(100) NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT NULL,
+	CONSTRAINT fee_types_pk PRIMARY KEY (fee_id)
 );
 
 
-CREATE TABLE `sgst_rates` ( 
-	`GST_RATE`	double not NULL,
-	`NAME`    	varchar(100) NULL,
-	PRIMARY KEY(`GST_RATE`)
+CREATE TABLE smartschoolingdev.fee_details (
+	fee_id varchar(100) NOT NULL,
+	class_id varchar(100),
+	route_id varchar(100),
+	amount DECIMAL NOT NULL,
+	eff_date DATETIME NOT NULL,
+	end_date DATETIME NOT NULL,
+	CONSTRAINT fee_details_pk PRIMARY KEY (fee_id,class_id,route_id,amount,eff_date,end_date),
+	CONSTRAINT fee_details_fk_2 FOREIGN KEY (fee_id) REFERENCES smartschoolingdev.fee_types(fee_id)
 );
 
-
-CREATE TABLE `igst_rates` ( 
-	`GST_RATE`	double not NULL,
-	`NAME`    	varchar(100) NULL,
-	PRIMARY KEY(`GST_RATE`)
+CREATE TABLE smartschoolingdev.academic_details (
+	academic_id varchar(100) NOT NULL,
+	academic_year varchar(100) NOT NULL,
+	display_name varchar(100) NOT NULL,
+	academic_start_date DATETIME NOT NULL,
+	academic_end_date DATETIME NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_update_user varchar(100) NOT NULL,
+	CONSTRAINT academic_details_pk PRIMARY KEY (academic_id),
+	CONSTRAINT academic_details_un UNIQUE KEY (academic_year,display_name,academic_id,academic_start_date,academic_end_date)
 );
 
-ALTER TABLE product_det CHANGE `RF1` `UNIT` varchar(200);
-
-
-CREATE TABLE `product_sgst_map` ( 
-	`PRODUCT_ID`      	INT not NULL,
-	`GST_RATE`        	double not NULL,
-	`EFF_DATE_SKEY`   	integer(11) not NULL,
-	`END_DATE_SKEY`   	integer(11) not NULL,
-	`LAST_UPDATE_TIME`	datetime NULL,
-	PRIMARY KEY(`PRODUCT_ID`,`GST_RATE`,`EFF_DATE_SKEY`,`END_DATE_SKEY`)
-);
-ALTER TABLE `product_sgst_map`
-	ADD CONSTRAINT `FK16`
-	FOREIGN KEY(`PRODUCT_ID`)
-	REFERENCES `product_det`(`PRODUCT_ID`);
-
-
-CREATE TABLE `product_cgst_map` ( 
-	`PRODUCT_ID`      	INT not NULL,
-	`GST_RATE`        	double not NULL,
-	`EFF_DATE_SKEY`   	integer(11) not NULL,
-	`END_DATE_SKEY`   	integer(11) not NULL,
-	`LAST_UPDATE_TIME`	datetime NULL,
-	PRIMARY KEY(`PRODUCT_ID`,`GST_RATE`,`EFF_DATE_SKEY`,`END_DATE_SKEY`)
-);
-ALTER TABLE `product_cgst_map`
-	ADD CONSTRAINT `FK15`
-	FOREIGN KEY(`PRODUCT_ID`)
-	REFERENCES `product_det`(`PRODUCT_ID`);
-
-
-CREATE TABLE `product_igst_map` ( 
-	`PRODUCT_ID`      	INT not NULL,
-	`GST_RATE`        	double not NULL,
-	`EFF_DATE_SKEY`   	integer(11) not NULL,
-	`END_DATE_SKEY`   	integer(11) not NULL,
-	`LAST_UPDATE_TIME`	datetime not NULL,
-	PRIMARY KEY(`PRODUCT_ID`,`GST_RATE`,`EFF_DATE_SKEY`,`END_DATE_SKEY`)
-);
-ALTER TABLE `product_igst_map`
-	ADD CONSTRAINT `FK`
-	FOREIGN KEY(`PRODUCT_ID`)
-	REFERENCES `product_det`(`PRODUCT_ID`);
-
-
-CREATE TABLE `vendor_det` ( 
-	`VENDOR_ID`  	INT NOT NULL,
-	`VENDOR_NAME`	varchar(700) NOT NULL,
-	`CITY`       	varchar(500) NULL,
-	`DISTRICT`   	varchar(500) NULL,
-	`STATE`      	varchar(500) NULL,
-	`PHONE`      	varchar(12) NULL,
-	`EMAIL`      	varchar(500) NULL,
-	`GST_NO`     	varchar(100) NULL,
-	`RF1`        	integer(11) NULL,
-	`RF2`        	integer(11) NULL,
-	`RF3`        	integer(11) NULL,
-	PRIMARY KEY(`VENDOR_ID`)
+CREATE TABLE smartschoolingdev.class_subject_details (
+	class_id varchar(100) NOT NULL,
+	sub_id varchar(100) NOT NULL,
+	eff_date DATETIME NOT NULL,
+	end_date DATETIME NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT NULL,
+	CONSTRAINT class_subject_details_pk PRIMARY KEY (class_id,sub_id,eff_date,end_date),
+	CONSTRAINT class_subject_details_fk FOREIGN KEY (class_id) REFERENCES smartschoolingdev.classes(class_id),
+	CONSTRAINT class_subject_details_fk_1 FOREIGN KEY (sub_id) REFERENCES smartschoolingdev.subjects(sub_id)
 );
 
-
-CREATE TABLE `vendor_receipt_map` ( 
-	`VENDOR_ID` 	INT not NULL,
-	`INVOICE_ID`	varchar(200) not NULL,
-	`DATE_SKEY`  	integer(11) not NULL,
-	PRIMARY KEY(`VENDOR_ID`,`INVOICE_ID`,`DATE_SKEY`)
+CREATE TABLE smartschoolingdev.student_details (
+	stud_id varchar(100) NOT NULL,
+	first_name varchar(100) NOT NULL,
+	middle_name varchar(100) NULL,
+	last_name varchar(100) NOT NULL,
+	birth_date DATETIME NOT NULL,
+	gender varchar(100) NULL,
+	adhar varchar(100) NOT NULL,
+	email varchar(100) NULL,
+	mobile varchar(100) NOT NULL,
+	alternate_mobile varchar(100) NULL,
+	address varchar(500) NOT NULL,
+	religion varchar(100) NULL,
+	caste varchar(100) NULL,
+	transport CHAR NOT NULL,
+	nationality varchar(100) NULL,
+	CONSTRAINT student_details_pk PRIMARY KEY (stud_id),
+	CONSTRAINT student_details_un UNIQUE KEY (adhar)
 );
 
-
-CREATE TABLE `purchase_order_det` ( 
-	`PRODUCT_ID` 	INT NOT NULL,
-	`INVOICE_ID` 	varchar(200) NOT NULL,
-	`BATCH_NO`   	varchar(200) not NULL,
-	`QUANTITY`   	integer(11) NULL,
-	`PRICE`      	double NULL,
-	`COST`       	double NULL,
-	`GST`        	double NULL,
-	`MFG_DATE`   	datetime NULL,
-	`EXP_DATE`   	datetime NULL,
-	`BEST_BEFORE`	varchar(300) NULL,
-	`RF1`        	integer(11) NULL,
-	`RF2`        	integer(11) NULL,	
-	`RF3`        	integer(11) NULL,
-	`RF4`        	integer(11) NULL,
-	`RF5`        	integer(11) NULL,
-	PRIMARY KEY(`PRODUCT_ID`,`INVOICE_ID`,`BATCH_NO`)
+CREATE TABLE smartschoolingdev.student_fees_details (
+	stud_id varchar(100) NOT NULL,
+	academic_id varchar(100) NOT NULL,
+	fee_id varchar(100) NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT NULL,
+	CONSTRAINT student_fees_details_pk PRIMARY KEY (stud_id,academic_id,fee_id),
+	CONSTRAINT student_fees_details_fk FOREIGN KEY (stud_id) REFERENCES smartschoolingdev.student_details(stud_id),
+	CONSTRAINT student_fees_details_fk_1 FOREIGN KEY (fee_id) REFERENCES smartschoolingdev.fee_types(fee_id),
+	CONSTRAINT student_fees_details_fk_2 FOREIGN KEY (academic_id) REFERENCES smartschoolingdev.academic_details(academic_id)
 );
 
-ALTER TABLE `purchase_order_det`
-	ADD CONSTRAINT `FK19`
-	FOREIGN KEY(`PRODUCT_ID`)
-	REFERENCES `product_det`(`PRODUCT_ID`);
-
-RENAME TABLE `sgst_rates` TO `gst_rates`;
-
-ALTER TABLE `purchase_order_det` MODIFY COLUMN `QUANTITY` double(16,4) NULL;
-
-ALTER TABLE `purchase_order_det` MODIFY COLUMN `RF1` double(16,4) NOT NULL;
-
-ALTER TABLE `purchase_order_det` CHANGE COLUMN `RF1` `IN_STOCK` double(16,4) NOT NULL;
-
-ALTER TABLE vendor_det MODIFY COLUMN `GST_NO` VARCHAR NULL;
-
-alter table vendor_receipt_map add column DUE_AMT double null;
-
-alter table vendor_receipt_map add column TOTAL_AMT double null;
-
-alter table vendor_receipt_map add column DISCOUNT double null;
-
-alter table vendor_receipt_map add column PAYMENT_TYPE VARCHAR(200) null;
-
-alter table vendor_receipt_map add column CHEQUE_NO VARCHAR(200) null;
-
-alter table vendor_receipt_map add column GST_AMT double null;
-
-alter table vendor_receipt_map add column ACC_NO varchar(200) null;
-
-create table PRODUCT_STOCK_MAP(
-	PRODUCT_ID 	INT NOT NULL,
-	AVAIL_STOCK INT not null,
-	LAST_UPDATE_TIME datetime null,
-	LAST_USER varchar(200) null,
-	
-	primary key(PRODUCT_ID)
-);
-ALTER TABLE PRODUCT_STOCK_MAP
-	ADD CONSTRAINT `FK20`
-	FOREIGN KEY(`PRODUCT_ID`)
-	REFERENCES `product_det`(`PRODUCT_ID`);
-
-alter table customer_det drop column address;
-
-alter table customer_det add column village varchar(200) null;
-alter table customer_det add column city varchar(200) null;
-alter table customer_det add column district varchar(200) null;
-alter table customer_det add column state varchar(200) null;
-alter table customer_det add column PIN_CODE varchar(200) null;
-alter table customer_det add column UID_NO varchar(200) null;
-alter table customer_det add column GST_NO varchar(200) null;
-alter table customer_det add column PAN_NO varchar(200) null;
-
-alter table vendor_det add column village varchar(200) null;
-alter table vendor_det add column PIN_CODE varchar(200) null;
-alter table vendor_det add column UID_NO varchar(200) null;
-alter table vendor_det add column PAN_NO varchar(200) null;
-
-alter table order_customer_map add column DUE_AMT double null;
-
-alter table order_customer_map add column TOTAL_AMT double null;
-
-alter table order_customer_map add column DISCOUNT double null;
-
-alter table order_customer_map add column PAYMENT_TYPE VARCHAR(200) null;
-
-alter table order_customer_map add column CHEQUE_NO VARCHAR(200) null;
-
-alter table order_customer_map add column GST_AMT double null;
-
-alter table order_customer_map add column ACC_NO varchar(200) null;
-
-
-alter table order_det add column BATCH_NO VARCHAR(200) null;
-
-alter table order_det add column RATE double null;
-
-alter table order_customer_map rename column CUSTOMET_ID to CUSTOMER_ID
-
-ALTER TABLE product_det CHANGE `RF2` `MRP` double null;
-ALTER TABLE product_det CHANGE `RF3` `MANUFACTURER` varchar(200) null;
-ALTER TABLE product_det CHANGE `RF4` `SELLING_PRICE` double null;
-ALTER TABLE product_det CHANGE `RF5` `PACKAGING` varchar(200) null;
-
-create table alerts(
-	ALERT_ID int not null,
-	ALERT_NAME varchar(1000) not null,
-	ALERT_TYPE char not null,
-	ALERT_DISCRIPTION varchar(500) not null,
-	
-	primary key(ALERT_ID)
+CREATE TABLE smartschoolingdev.student_transport_details (
+	stud_id varchar(100) NOT NULL,
+	route_id varchar(100) NOT NULL,
+	eff_date DATETIME NOT NULL,
+	end_date DATETIME NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT NULL,
+	CONSTRAINT student_transport_detaisl_pk PRIMARY KEY (stud_id,route_id,eff_date,end_date),
+	CONSTRAINT student_transport_detaisl_fk FOREIGN KEY (stud_id) REFERENCES smartschoolingdev.student_details(stud_id),
+	CONSTRAINT student_transport_detaisl_fk_1 FOREIGN KEY (route_id) REFERENCES smartschoolingdev.routes(route_id)
 );
 
-ALTER TABLE order_customer_map CHANGE `CUSTOMET_ID` `CUSTOMER_ID` int NOT NULL;
+CREATE TABLE smartschoolingdev.student_class_details (
+	stud_id varchar(100) NOT NULL,
+	academic_id varchar(100) NOT NULL,
+	class_id varchar(100) NOT NULL,
+	last_update_time DATETIME NOT NULL,
+	last_user varchar(100) NOT NULL
+);
+ALTER TABLE smartschoolingdev.student_class_details ADD CONSTRAINT student_class_details_pk PRIMARY KEY (stud_id,academic_id,class_id);
+ALTER TABLE smartschoolingdev.student_class_details ADD CONSTRAINT student_class_details_fk FOREIGN KEY (class_id) REFERENCES smartschoolingdev.classes(class_id);
+ALTER TABLE smartschoolingdev.student_class_details ADD CONSTRAINT student_class_details_fk_1 FOREIGN KEY (stud_id) REFERENCES smartschoolingdev.student_details(stud_id);
+ALTER TABLE smartschoolingdev.student_class_details ADD CONSTRAINT student_class_details_fk_2 FOREIGN KEY (academic_id) REFERENCES smartschoolingdev.academic_details(academic_id);
 
-delete from alerts where `ALERT_ID` = 1;
-INSERT INTO alerts
-(`ALERT_ID`, `ALERT_NAME`, `ALERT_TYPE`, `ALERT_DISCRIPTION`)
-VALUES(1, 'PUR_ORDR_SUCCESS', 'S', 'Purchase Order Addedd Successfully');
-
-delete from alerts where `ALERT_ID` = 2;
-INSERT INTO alerts
-(`ALERT_ID`, `ALERT_NAME`, `ALERT_TYPE`, `ALERT_DISCRIPTION`)
-VALUES(2, 'PUR_ORDR_ERROR', 'E', 'Error in adding Purchase Order');
-
-delete from alerts where `ALERT_ID` = 3;
-INSERT INTO alerts
-(`ALERT_ID`, `ALERT_NAME`, `ALERT_TYPE`, `ALERT_DISCRIPTION`)
-VALUES(3, 'VEN_RECPT_MAP_ERROR', 'E', 'Error in adding vendor receipt map');
-
-delete from alerts where `ALERT_ID` = 4;
-INSERT INTO bni.alerts
-(`ALERT_ID`, `ALERT_NAME`, `ALERT_TYPE`, `ALERT_DISCRIPTION`)
-VALUES(4, 'PROD_STOCK_MAP_UPDATE_ERROR', 'E', 'Error in updating product stock map');
-
-ALTER TABLE order_det CHANGE `RF1` `GST` double null;
-
-ALTER TABLE order_det CHANGE `RF2` `EXP_DATE` datetime null;
